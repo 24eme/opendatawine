85dfc15278dbd2d094a2a2541eb82efbf885935f
Tangui Morlier <tangui@tangui.eu.org>
2022-04-20 19:26:34 +0200

    Génération des geojsons, ajout du cadastre + génération des pages statisques

--- bin/get_delimination_aoc.sh
+++ bin/get_delimination_aoc.sh
@@ -42,3 +42,44 @@ done  | sed 's/,.*//' | sort -u | while read iddenom; do
         echo >> $file
     done
 done
+
+ls -d delimitation_aoc/*/*/ | while read villedir ; do
+    commune=$(echo $villedir | awk -F '/' '{print $3}')
+    departement=$(echo $villedir | awk -F '/' '{print $2}')
+    cd $villedir;
+    echo '{' > delimitations.json
+    ls *geojson | while read geojson; do
+        echo -n '"';
+        sed 's/.*"denom":"//' $geojson | sed 's/".*//' | tr -d '\n';
+        echo -n '":"';
+        printf "%05d.geojson\"," $(sed 's/.*"id_denom"://' $geojson | sed 's/,.*//');
+        echo
+    done >> delimitations.json
+    sed -i '$ s/,$//' delimitations.json
+    echo '}' >> delimitations.json
+    curl -s "https://cadastre.data.gouv.fr/data/etalab-cadastre/latest/geojson/communes/"$departement"/"$commune"/cadastre-"$commune"-parcelles.json.gz" | zcat > cadastre-parcelles.json
+    cd - > /dev/null
+done
+
+echo "dt,type_prod,categorie,type_denom,type_ig,id_app,app,id_denom,denom,insee,nomcom,insee2011,nomcom2011,id_aire,crinao,grp_name1,grp_name2" > denominations.csv
+cat $(find delimitation_aoc/ -name denominations.json) | grep '^"' | sort -u | sed 's/.geojson//' | awk -F '"' '{print $4";"$2}' | while read line; do
+    denomid=$(echo $line| sed 's/;.*//')
+    denomination=$(echo $line| sed 's/.*;//')
+    file=$(find delimitation_aoc -name $denomid".geojson" | head -n 1)
+    jq -c .features[0].properties < $file | sed 's/\\"//g' | sed 's/,[^:,]*:/:/g' | sed 's/}/:/' | awk -F ':' '{print $2";"$3";"$4";"$5";"$6";"$7";"$8";"$9";"$10";"$11";"$12";"$13";"$14";"$15";"$16";"$17";"$18}' >> denominations.csv
+done
+
+echo "<html><body><h1>Dénomination INAO</h1><ul>" > denominations.html
+tail -n +2 denominations.csv | sed 's/"//g' | awk -F ';' '{printf("<li><a href=\"denominations/%05d.html\">%s</a></li>\n", $8, $9);}' >> denominations.html
+echo "</ul></body></html>" >> denominations.html
+
+tail -n +2 denominations.csv | awk -F ';' '{printf("%05d;%s\n", $8, $9);}' | sed 's/"//g' | while read line ; do
+    denomid=$(echo $line | sed 's/;.*//')
+    denomination=$(echo $line | sed 's/.*;//')
+    echo "<html><body><h1>"$denomination"</h1><p>Liste des villes:</p><table>" > "denominations/"$denomid".html"
+    find . -name $denomid'.geojson' | while read geo ; do
+        cat $geo | sed 's/.*"insee"://' | sed 's/insee2011".*//' | awk -F '"' '{dep=substr($2,0,2); print "<tr><td>"dep"</td><td><a href=\"../carte.html?insee="$2"&denomid='$denomid'\">"$6"</a></td></tr>"}'
+    done >> "denominations/"$denomid".html"
+    echo "</table></body></html>" >> "denominations/"$denomid".html"
+    echo "denominations/"$denomid".html"
+done
