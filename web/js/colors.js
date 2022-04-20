2a7354405618ee4f23f0fb3b2ef2772f21cfb8b5
Tangui Morlier <tangui@tangui.eu.org>
2022-04-20 19:22:49 +0200

    Listing des dénominations + carte ign

---
+++ web/js/colors.js
@@ -0,0 +1,152 @@
+var colors = [
+    "#E53935", //Red 600
+    "#FDD835", //Yellow 600
+    "#D81B60", //Pink 600
+    "#8E24AA", //Purple 600
+    "#43A047", //Green 600
+    "#6D4C41", //Brown 600
+    "#1E88E5", //Blue 600
+    "#00ACC1", //Cyan 600
+    "#7CB342", //Light Green 600
+    "#C0CA33", //Lime 600
+    "#FFB300", //Amber 600
+    "#FB8C00", //Orange 600
+    "#5E35B1", //Deep Purple 600
+    "#00897B", //Teal 600
+    "#F4511E", //Deep Orange 600
+    "#3949AB", //Indigo 600
+    "#039BE5", //Light Blue 600
+    "#757575", //Grey 600
+    "#546E7A", //Blue Grey 600
+    "#C62828", //Red 800
+    "#F9A825", //Yellow 800
+    "#AD1457", //Pink 800
+    "#6A1B9A", //Purple 800
+    "#2E7D32", //Green 800
+    "#4E342E", //Brown 800
+    "#1565C0", //Blue 800
+    "#00838F", //Cyan 800
+    "#558B2F", //Light Green 800
+    "#9E9D24", //Lime 800
+    "#FF8F00", //Amber 800
+    "#EF6C00", //Orange 800
+    "#4527A0", //Deep Purple 800
+    "#00695C", //Teal 800
+    "#D84315", //Deep Orange 800
+    "#283593", //Indigo 800
+    "#0277BD", //Light Blue 800
+    "#37474F", //Blue Grey 800
+    "#EF5350", //Red 400
+    "#FFEE58", //Yellow 400
+    "#EC407A", //Pink 400
+    "#AB47BC", //Purple 400
+    "#66BB6A", //Green 400
+    "#8D6E63", //Brown 400
+    "#42A5F5", //Blue 400
+    "#26C6DA", //Cyan 400
+    "#9CCC65", //Light Green 400
+    "#D4E157", //Lime 400
+    "#FFCA28", //Amber 400
+    "#FFA726", //Orange 400
+    "#7E57C2", //Deep Purple 400
+    "#26A69A", //Teal 400
+    "#FF7043", //Deep Orange 400
+    "#5C6BC0", //Indigo 400
+    "#29B6F6", //Light Blue 400
+    "#BDBDBD", //Grey 400
+    "#78909C", //Blue Grey 400
+    "#EF9A9A", //Red 200
+    "#FFF59D", //Yellow 200
+    "#F48FB1", //Pink 200
+    "#CE93D8", //Purple 200
+    "#A5D6A7", //Green 200
+    "#BCAAA4", //Brown 200
+    "#90CAF9", //Blue 200
+    "#80DEEA", //Cyan 200
+    "#C5E1A5", //Light Green 200
+    "#E6EE9C", //Lime 200
+    "#FFE082", //Amber 200
+    "#FFCC80", //Orange 200
+    "#B39DDB", //Deep Purple 200
+    "#80CBC4", //Teal 200
+    "#FFAB91", //Deep Orange 200
+    "#9FA8DA", //Indigo 200
+    "#81D4FA", //Light Blue 200
+    "#B0BEC5", //Blue Grey 200
+    "#B71C1C", //Red 900
+    "#F57F17", //Yellow 900
+    "#880E4F", //Pink 900
+    "#4A148C", //Purple 900
+    "#1B5E20", //Green 900
+    "#3E2723", //Brown 900
+    "#0D47A1", //Blue 900
+    "#006064", //Cyan 900
+    "#33691E", //Light Green 900
+    "#827717", //Lime 900
+    "#FF6F00", //Amber 900
+    "#E65100", //Orange 900
+    "#311B92", //Deep Purple 900
+    "#004D40", //Teal 900
+    "#BF360C", //Deep Orange 900
+    "#1A237E", //Indigo 900
+    "#01579B", //Light Blue 900
+    "#212121", //Grey 900
+    "#E57373", //Red 300
+    "#FFF176", //Yellow 300
+    "#F06292", //Pink 300
+    "#BA68C8", //Purple 300
+    "#81C784", //Green 300
+    "#A1887F", //Brown 300
+    "#64B5F6", //Blue 300
+    "#4DD0E1", //Cyan 300
+    "#AED581", //Light Green 300
+    "#DCE775", //Lime 300
+    "#FFD54F", //Amber 300
+    "#FFB74D", //Orange 300
+    "#9575CD", //Deep Purple 300
+    "#4DB6AC", //Teal 300
+    "#FF8A65", //Deep Orange 300
+    "#7986CB", //Indigo 300
+    "#4FC3F7", //Light Blue 300
+    "#90A4AE", //Blue Grey 300
+    "#D32F2F", //Red 700
+    "#FBC02D", //Yellow 700
+    "#C2185B", //Pink 700
+    "#7B1FA2", //Purple 700
+    "#388E3C", //Green 700
+    "#5D4037", //Brown 700
+    "#1976D2", //Blue 700
+    "#0097A7", //Cyan 700
+    "#689F38", //Light Green 700
+    "#AFB42B", //Lime 700
+    "#FFA000", //Amber 700
+    "#F57C00", //Orange 700
+    "#512DA8", //Deep Purple 700
+    "#00796B", //Teal 700
+    "#E64A19", //Deep Orange 700
+    "#303F9F", //Indigo 700
+    "#0288D1", //Light Blue 700
+    "#616161", //Grey 700
+    "#455A64", //Blue Grey 700
+    "#FFCDD2", //Red 100
+    "#FFF9C4", //Yellow 100
+    "#F8BBD0", //Pink 100
+    "#E1BEE7", //Purple 100
+    "#C8E6C9", //Green 100
+    "#D7CCC8", //Brown 100
+    "#BBDEFB", //Blue 100
+    "#B2EBF2", //Cyan 100
+    "#DCEDC8", //Light Green 100
+    "#F0F4C3", //Lime 100
+    "#FFECB3", //Amber 100
+    "#FFE0B2", //Orange 100
+    "#D1C4E9", //Deep Purple 100
+    "#B2DFDB", //Teal 100
+    "#FFCCBC", //Deep Orange 100
+    "#C5CAE9", //Indigo 100
+    "#B3E5FC", //Light Blue 100
+    "#CFD8DC"  //Blue Grey 100
+];
+function getColorById(i) {
+    return colors[i];
+}
