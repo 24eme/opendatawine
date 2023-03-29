241977ea96c2ca87497d2192b27fc75fd5145b47
Tangui Morlier <tangui@tangui.eu.org>
2023-03-29 17:34:17 +0200

    Aide à la mise à jour du site opendatawine.fr

---
+++ bin/publish_to_githubpages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+hashgithubpages=$(git log --oneline githubpages | head -n 1 | awk '{print $1}')
+hashmaster=$(git log --oneline master | head -n 1 | awk '{print $1}')
+git switch githubpages
+git reset $hashmaster
+git checkout bin carte.html CNAME communes.html denominations.csv denominations.html index.html LICENSE
+git reset $hashgithubpages
+git add bin carte.html CNAME communes.html denominations.csv denominations.html index.html LICENSE
+git commit -m 'Manual merge branch master ('$hashmaster') into githubpages'
+git switch master
+
