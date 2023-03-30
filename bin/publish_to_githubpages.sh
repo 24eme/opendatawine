3baab403d2f0bcec83e216e870e02b9219ee323d
Tangui Morlier <tangui@tangui.eu.org>
2023-03-30 10:56:55 +0200

    meilleure technique de publication

--- bin/publish_to_githubpages.sh
+++ bin/publish_to_githubpages.sh
@@ -4,8 +4,10 @@ hashgithubpages=$(git log --oneline githubpages | head -n 1 | awk '{print $1}')
 hashmaster=$(git log --oneline master | head -n 1 | awk '{print $1}')
 git switch githubpages
 git reset $hashmaster
-git checkout bin carte.html CNAME communes.html denominations.csv denominations.html index.html LICENSE
+git checkout bin carte.html CNAME communes.html denominations denominations.csv denominations.html index.html LICENSE
+rm -rf delimitation_aoc
 git reset $hashgithubpages
+git checkout .gitignore
 git add bin carte.html CNAME communes.html denominations.csv denominations.html index.html LICENSE
 git commit -m 'Manual merge branch master ('$hashmaster') into githubpages'
 git switch master
